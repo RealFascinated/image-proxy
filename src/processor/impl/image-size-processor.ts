5f49fcb37b551f8b9f0e824f05d65370e80d206d
fascinated <liam@fascinated.cc>
2025-06-06 09:42:38 +0100

    fixed

--- src/processor/impl/image-size-processor.ts
+++ src/processor/impl/image-size-processor.ts
@@ -4,10 +4,22 @@ import { Processor } from "../processor";
 
 export class SizeProcessor extends Processor {
   public canRun(options: ImageOptions) {
-    return !!(options.width && options.height);
+    return !!(options.width || options.height);
   }
 
   public async run(options: ImageOptions, image: Sharp) {
-    return image.resize(options.width, options.height);
+    const metadata = await image.metadata();
+    const aspectRatio = metadata.width! / metadata.height!;
+
+    let width = options.width;
+    let height = options.height;
+
+    if (width && !height) {
+      height = Math.round(width / aspectRatio);
+    } else if (!width && height) {
+      width = Math.round(height * aspectRatio);
+    }
+
+    return image.resize(width, height);
   }
 }
